5765ee559946403cdeb1b92a78515d3ad77ad244
Dilip  Kumar Yadav <111607936+yadavDilip7@users.noreply.github.com>
2023-04-19 07:51:55 +0530

    Add files via upload

---
+++ 06 Shift Reduce Parsing/SRP.C
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int z, i, j, c;
+char a[16], stk[15];
+
+void reduce();
+
+void main() {
+	clrscr();
+	puts("GRAMMAR is \n E->E+E \n E->E*E \n E->(E) \n E->a");
+	puts("Enter input string: ");
+	gets(a);
+	c = strlen(a);
+	a[c] = '$';
+	stk[0] = '$';
+	puts("stack \t input \t action");
+	for(i = 1, j = 0; j < c; i++, j++) {
+		stk[i] = a[j];
+		stk[i + 1] = '\0';
+		a[j] = ' ';
+		printf("\n%s\t%s\tshift %c", stk, a, stk[i]);
+		reduce();
+	}
+	if(a[j] == '$')
+		reduce();
+	if((stk[1] == 'E') && (stk[2] == '\0'))
+		printf("\n%s\t%s\tAccept", stk, a);
+	else
+		printf("\n%s\t%s\terror", stk, a);
+	getch();
+}
+void reduce() {
+	for(z = 1; z <= c; z++) {
+		if(stk[z] == 'a') {
+			stk[z] = 'E';
+			stk[z + 1] = '\0';
+			printf("\n%s\t%s\tReduce by E->a", stk, a);
+			reduce();
+		}
+	}
+	for(z = 1; z <= c; z++) {
+		if(stk[z] == 'E' && stk[z + 1] == '+' && stk[z + 2] == 'E') {
+			stk[z] = 'E';
+			stk[z + 1] = '\0';
+			stk[z + 2] = '\0';
+			printf("\n%s\t%s\tReduce by E->E+E", stk, a);
+			i = i - 2;
+			reduce();
+		}
+	}
+	for(z = 1; z <= c; z++) {
+		if(stk[z] == 'E' && stk[z + 1] == '*' && stk[z + 2] == 'E') {
+			stk[z] = 'E';
+			stk[z + 1] = '\0';
+			stk[z + 2] = '\0';
+			printf("\n%s\t%s\tReduce by E->E*E", stk, a);
+			i = i - 2;
+			reduce();
+		}
+	}
+	for(z = 1; z <= c; z++) {
+		if(stk[z] == '(' && stk[z + 1] == 'E' && stk[z + 2] == ')') {
+			stk[z] = 'E';
+			stk[z + 1] = '\0';
+			stk[z + 2] = '\0';
+			printf("\n%s\t%s\tReduce by E->(E)", stk, a);
+			i = i - 2;
+			reduce();
+		}
+	}
+}
+
+/*
+OUTPUT:
+GRAMMAR is                                                                      
+ E->E+E                                                                         
+ E->E*E                                                                         
+ E->(E)                                                                         
+ E->a                                                                           
+Enter input string:                                                             
+a*a+a                                                                           
+stack    input   action                                                         
+                                                                                
+$a       *a+a$  shift a                                                         
+$E       *a+a$  Reduce by E->a                                                  
+$E*       a+a$  shift->*                                                        
+$E*a       +a$  shift a                                                         
+$E*E       +a$  Reduce by E->a                                                  
+$E         +a$  Reduce by E->E*E                                                
+$E+         a$  shift->+                                                        
+$E+a         $  shift a                                                         
+$E+E         $  Reduce by E->a                                                  
+$E           $  Reduce by E->E+E                                                
+$E           $  Accept                                                          
+*/
